25eae9ffc7b840eaa3ea8e65f622ee6179f1a0fd
Yash Singhal <y.yashsinghal207@gmail.com>
2026-07-05 19:06:30 +0530

    Time: 3 ms (36.4%), Space: 113.8 MB (27.49%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -5,13 +5,12 @@ public:
         for(int i=0;i<k;i++){
             sum += nums[i];
         }
-        double maxs = sum / k;
-        double avg = 0.00000;
-        for(int i =k;i<nums.size();i++){
+        double maxa = sum / k;
+        for(int i=k;i<nums.size();i++){
             sum = sum - nums[i-k] + nums[i];
-            avg = sum / k;
-            maxs = max(maxs,avg);
+            double avg = sum / k;
+            maxa = max(maxa,avg);
         }
-        return maxs;
+        return maxa;
     }
 };
